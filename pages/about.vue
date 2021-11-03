8663015f7d6c0509330e42775e83e0b5a568b6d1
Alemoh Rapheal <raphealenike@gmail.com>
2021-11-03 08:15:39 -0700

    App built left with touches on the UI using vuetifyjs

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-container>
+      <h2>About News App</h2>
+      <v-layout>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, aliquam, culpa autem quos quaerat optio quibusdam quod laborum eveniet recusandae expedita laudantium consequatur repellat inventore ipsa incidunt accusamus sapiente hic sed aut veritatis blanditiis nemo itaque neque. Temporibus, a dolorem veritatis nihil praesentium explicabo ut aperiam placeat ex eveniet, cupiditate laudantium, deserunt eos consequatur ipsa sapiente culpa nobis minima neque tempora! Quibusdam magnam temporibus fugit, error non reprehenderit illo minus dolorum praesentium quam mollitia itaque sint, repellat voluptates, inventore velit iusto. Aliquam ducimus sint tempore repellendus minima repellat corporis quasi unde natus voluptate fugiat perferendis, dolorem perspiciatis ut deserunt magni.
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/news.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-container>
+      <v-flex xs12 pb-5>
+        <h1>News</h1>
+      </v-flex>
+
+      <v-flex>
+        <v-flex
+          xs12
+          sm6
+          offset-sm3
+          pb-3
+          v-for="(article, index) in articles"
+          :key="index"
+        >
+          <v-card>
+            <img
+              class="white--text align-end"
+              height="200px"
+              width="100"
+              :src="article.urlToImage"
+            />
+
+            <v-card-subtitle class="pb-0">
+              {{ article.title }}
+            </v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>{{ article.content }}</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="orange" text flat :href="article.url" target="_blank"> Explore </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-flex>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      "https://newsapi.org/v2/everything?q=tesla&from=2021-10-03&sortBy=publishedAt&apiKey=ee346198c2d44fd6870f5b8fb21a25ba"
+    );
+    return { articles };
+  },
+};
+</script>
+
+<style>
+</style>
